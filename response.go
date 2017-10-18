4dca51e4f658a2a99c06da95ac464adb551c098e
Andy Whittle <andy.whittle@fatsoma.com>
2017-10-18 21:29:02 +0100

    Converting response struct to use JSON types

--- response.go
+++ response.go
@@ -3,32 +3,34 @@ package hpp
 import "strings"
 
 type Response struct {
+	hpp *HPP
+
 	// This is the merchant id that Realex Payments assign to you.
-	MerchantID string `json:"MERCHANT_ID"`
+	MerchantID JSONString `json:"MERCHANT_ID"`
 
 	// The sub-account used in the transaction.
-	Account string `json:"ACCOUNT"`
+	Account JSONString `json:"ACCOUNT"`
 
 	// The unique order id that you sent to us.
-	OrderID string `json:"ORDER_ID"`
+	OrderID JSONString `json:"ORDER_ID"`
 
 	// The amount that was authorised. Returned in the lowest unit of the currency.
-	Amount int `json:"AMOUNT,string"`
+	Amount JSONInt `json:"AMOUNT,string"`
 
 	// Will contain a valid authcode if the transaction was successful. Will be empty otherwise.
-	AuthCode string `json:"AUTHCODE"`
+	AuthCode JSONString `json:"AUTHCODE"`
 
 	// The date and time of the transaction.
 	TimeStamp *JSONTime `json:"TIMESTAMP"`
 
 	// A SHA-1 digital signature created using the HPP response fields and your shared secret.
-	Hash string `json:"SHA1HASH"`
+	Hash JSONString `json:"SHA1HASH"`
 
 	// The outcome of the transaction. Will contain "00" if the transaction was a success or another value (depending on the error) if not.
-	Result string `json:"RESULT"`
+	Result JSONString `json:"RESULT"`
 
 	// Will contain a text message that describes the result code.
-	Message string `json:"MESSAGE"`
+	Message JSONString `json:"MESSAGE"`
 
 	// The result of the Card Verification check (if enabled):
 	//
@@ -37,30 +39,30 @@ type Response struct {
 	// I: CVV Not checked due to circumstances.
 	// U: CVV Not checked - issuer not certified.
 	// P: CVV Not Processed.
-	CvnResult string `json:"CVNRESULT"`
+	CvnResult JSONString `json:"CVNRESULT"`
 
 	// A unique reference that Realex Payments assign to your transaction.
-	PasRef string `json:"PASREF"`
+	PasRef JSONString `json:"PASREF"`
 
 	// This is the Realex Payments batch that this transaction will be in.
 	// (This is equal to "-1" if the transaction was sent in with the autosettle flag off.
 	// After you settle it (either manually or programmatically) the response to that transaction will contain the batch id.)
-	BatchID string `json:"BATCHID"`
+	BatchID JSONString `json:"BATCHID"`
 
 	// This is the ecommerce indicator (this will only be returned for 3DSecure transactions).
-	ECI string `json:"ECI"`
+	ECI JSONString `json:"ECI"`
 
 	// Cardholder Authentication Verification Value (this will only be returned for 3DSecure transactions).
-	CAVV string `json:"CAVV"`
+	CAVV JSONString `json:"CAVV"`
 
 	// Exchange Identifier (this will only be returned for 3DSecure transactions).
-	XID string `json:"XID"`
+	XID JSONString `json:"XID"`
 
 	// Whatever data you have sent in the request will be returned to you.
-	CommentOne string `json:"COMMENT1"`
+	CommentOne JSONString `json:"COMMENT1"`
 
 	// Whatever data you have sent in the request will be returned to you.
-	CommentTwo string `json:"COMMENT2"`
+	CommentTwo JSONString `json:"COMMENT2"`
 
 	// The Transaction Suitability Score for the transaction. The RealScore is comprised of various distinct tests.
 	// Using the RealControl application you can request that Realex Payments return certain individual scores to you.
@@ -81,12 +83,12 @@ func (r *Response) BuildHash(secret string) string {
 
 	s := []string{
 		ts,
-		r.MerchantID,
-		r.OrderID,
-		r.Result,
-		r.Message,
-		r.PasRef,
-		r.AuthCode,
+		r.MerchantID.String(),
+		r.OrderID.String(),
+		r.Result.String(),
+		r.Message.String(),
+		r.PasRef.String(),
+		r.AuthCode.String(),
 	}
 
 	return GenerateHash(strings.Join(s, Separator), secret)
