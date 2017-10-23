7ea91f668bc5a03847cf64ae2b9d76a9e0c00456
Andy Whittle <andy.whittle@fatsoma.com>
2017-10-23 21:07:22 +0100

    Borrowing the comment / card payment button regexp from php sdk
    These regular expressions have been copied from the php sdk (https://github.com/realexpayments/rxp-hpp-php/blob/master/src/main/php/com-realexpayments-hpp-sdk/domain/HppRequest.php) as they are closer to the golang expression engine

--- validation.go
+++ validation.go
@@ -7,24 +7,24 @@ import (
 )
 
 var (
-	merchantIDRegexp     = regexp.MustCompile(`^[a-zA-Z0-9\.]*$`)
-	accountRegexp        = regexp.MustCompile(`^[a-zA-Z0-9\s]*$`)
-	orderIDRegexp        = regexp.MustCompile(`^[a-zA-Z0-9_\-]*$`)
-	numericRegexp        = regexp.MustCompile(`^[0-9]*$`)
-	alphaRegexp          = regexp.MustCompile(`^[a-zA-Z]*$`)
-	hexadecimalRegexp    = regexp.MustCompile(`^[0-9a-fA-F]+$`)
-	autoSettleFlagRegexp = regexp.MustCompile(`(?i)^on*|^off$|^*$|^multi$|^1$|^0$`)
-	// commentRegexp        = regexp.MustCompile(`^[\s \u0020-\u003B \u003D \u003F-\u007E \u00A1-\u00FF\u20AC\u201A\u0192\u201E\u2026\u2020\u2021\u02C6\u2030\u0160\u2039\u0152\u017D\u2018\u2019\u201C\u201D\u2022\u2013\u2014\u02DC\u2122\u0161\u203A\u0153\u017E\u0178]*$`)
-	boolRegexp         = regexp.MustCompile(`^[01]*$`)
-	payerExistsRegexp  = regexp.MustCompile(`^[012]*$`)
-	shippingCodeRegexp = regexp.MustCompile(`^[A-Za-z0-9\,\.\-\/\\| ]*$`)
-	countryRegexp      = regexp.MustCompile(`^[A-Za-z0-9\,\.\- ]*$`)
-	billingCodeRegexp  = regexp.MustCompile(`^[A-Za-z0-9\,\.\-\/\|\* ]*$`)
-	referenceRegexp    = regexp.MustCompile(`^[a-zA-Z0-9\.\_\-\,\+\@ \s]*$`)
-	languageRegexp     = regexp.MustCompile(`^[a-zA-Z]{2}$|^[a-zA-Z]{0}$`)
-	payerRegexp        = regexp.MustCompile(`^[A-Za-z0-9\_\-\\ ]*$`)
-	payRefRegexp       = regexp.MustCompile(`^[A-Za-z0-9\_\-]*$`)
-	// cardPaymentButtonRegexp = regexp.MustCompile(`^[ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖ×ØÙÚÛÜÝÞßàáâãäåæçèéêëìíîïðñòóôõö÷ø¤ùúûüýþÿ\\u0152\\u017D\\u0161\u0153\u017E\u0178¥a-zA-Z0-9\\'\\,\"\\+\\.\\_\\-\\&\\/\\@\\!\\?\\%\\()\\*\\:\\£\\$\\&\\u20AC\\#\\[\\]\\|\\=\\\\\u201C\u201D\u201C ]*$`)
+	merchantIDRegexp        = regexp.MustCompile(`^[a-zA-Z0-9\.]*$`)
+	accountRegexp           = regexp.MustCompile(`^[a-zA-Z0-9\s]*$`)
+	orderIDRegexp           = regexp.MustCompile(`^[a-zA-Z0-9_\-]*$`)
+	numericRegexp           = regexp.MustCompile(`^[0-9]*$`)
+	alphaRegexp             = regexp.MustCompile(`^[a-zA-Z]*$`)
+	hexadecimalRegexp       = regexp.MustCompile(`^[0-9a-fA-F]+$`)
+	autoSettleFlagRegexp    = regexp.MustCompile(`(?i)^on*|^off$|^*$|^multi$|^1$|^0$`)
+	commentRegexp           = regexp.MustCompile(`^[\s \x{0020}-\x{003B} \x{003D} \x{003F}-\x{007E} \x{00A1}-\x{00FF}\x{20AC}\x{201A}\x{0192}\x{201E}\x{2026}\x{2020}\x{2021}\x{02C6}\x{2030}\x{0160}\x{2039}\x{0152}\x{017D}\x{2018}\x{2019}\x{201C}\x{201D}\x{2022}\x{2013}\x{2014}\x{02DC}\x{2122}\x{0161}\x{203A}\x{0153}\x{017E}\x{0178}]*$`)
+	boolRegexp              = regexp.MustCompile(`^[01]*$`)
+	payerExistsRegexp       = regexp.MustCompile(`^[012]*$`)
+	shippingCodeRegexp      = regexp.MustCompile(`^[A-Za-z0-9\,\.\-\/\\| ]*$`)
+	countryRegexp           = regexp.MustCompile(`^[A-Za-z0-9\,\.\- ]*$`)
+	billingCodeRegexp       = regexp.MustCompile(`^[A-Za-z0-9\,\.\-\/\|\* ]*$`)
+	referenceRegexp         = regexp.MustCompile(`^[a-zA-Z0-9\.\_\-\,\+\@ \s]*$`)
+	languageRegexp          = regexp.MustCompile(`^[a-zA-Z]{2}$|^[a-zA-Z]{0}$`)
+	payerRegexp             = regexp.MustCompile(`^[A-Za-z0-9\_\-\\ ]*$`)
+	payRefRegexp            = regexp.MustCompile(`^[A-Za-z0-9\_\-]*$`)
+	cardPaymentButtonRegexp = regexp.MustCompile(`^[\x{00C0}\x{00C1}\x{00C2}\x{00C3}\x{00C4}\x{00C5}\x{00C6}\x{00C7}\x{00C8}\x{00C9}\x{00CA}\x{00CB}\x{00CC}\x{00CD}\x{00CE}\x{00CF}\x{00D0}\x{00D1}\x{00D2}\x{00D3}\x{00D4}\x{00D5}\x{00D6}\x{00D7}\x{00D8}\x{00D9}\x{00DA}\x{00DB}\x{00DC}\x{00DD}\x{00DE}\x{00DF}\x{00E0}\x{00E1}\x{00E2}\x{00E3}\x{00E4}\x{00E5}\x{00E6}\x{00E7}\x{00E8}\x{00E9}\x{00EA}\x{00EB}\x{00EC}\x{00ED}\x{00EE}\x{00EF}\x{00F0}\x{00F1}\x{00F2}\x{00F3}\x{00F4}\x{00F5}\x{00F6}\x{00F7}\x{00F8}\x{00A4}\x{00F9}\x{00FA}\x{00FB}\x{00FC}\x{00FD}\x{00FE}\x{00FF}\x{0152}\x{017D}\x{0161}\x{0153}\x{017E}\x{0178}\x{00A5}a-zA-Z0-9\'\,\+\x{0022}\.\_\-\&\/\@\!\?\%\()\*\:\x{00A3}\$\&\x{20AC}\#\[\]\|\=\\\x{201C}\x{201D}\x{201C} ]*$`)
 )
 
 const (
@@ -165,7 +165,7 @@ func validateComment(comment *string) *validation.FieldRules {
 	return validation.Field(
 		comment,
 		validation.Length(0, 255).Error(commentSize),
-		// validation.Match().Error(commentPattern),
+		validation.Match(commentRegexp).Error(commentPattern),
 	)
 }
 
@@ -236,7 +236,7 @@ func validateCardPaymentButton(cardPaymentButton *string) *validation.FieldRules
 	return validation.Field(
 		cardPaymentButton,
 		validation.Length(0, 25).Error(cardPaymentButtonTextSize),
-		// validation.Match(languageRegexp).Error(languagePattern),
+		validation.Match(cardPaymentButtonRegexp).Error(cardPaymentButtonTextPattern),
 	)
 }
 
